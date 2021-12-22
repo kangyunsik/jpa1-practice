4fcf1ade2f0a11ca174ce983af3e24f0a46f31f9
kangyunsik <re12io@ajou.ac.kr>
2021-12-23 02:11:18 +0900

    회원 기능 테스트

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception{
+        // GIVEN
+        Member member = new Member();
+        member.setName("kim");
+
+        // WHEN
+        memberService.join(member);
+        
+        // THEN
+        assertEquals(member, memberRepository.findOne(member.getId()));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception{
+        // GIVEN
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // WHEN
+        memberService.join(member1);
+        
+        // THEN
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+    }
+}
